df71e9ff41e2206b0e70184718db2e250852dc82
Jay Kate <jay.kate20@gmail.com>
2019-10-17 17:09:48 +0530

    V 0.1
    What's New ⏺
    1) Register Activity [Added]
    2) Login Activity [Added]
    3) Attendance Sheet Activity [Added]
    4) App Icon [Change]
    5) Login Account [Added]
    6) Firebase Services [Added]

---
+++ Android-Code/app/src/main/java/com/example/smartattendance/MainActivity.java
@@ -0,0 +1,97 @@
+package com.example.smartattendance;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText Name;
+    private EditText Password;
+    private Button Login;
+    private TextView Info;
+    private int counter = 5;
+    private TextView userRegistration;
+    private FirebaseAuth firebaseAuth;
+    private ProgressDialog progressDialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Name = (EditText)findViewById(R.id.etName);
+        Password = (EditText)findViewById(R.id.etPassword);
+        Login = (Button)findViewById(R.id.btnLogin);
+        Info = (TextView)findViewById(R.id.tvInfo);
+        userRegistration = (TextView)findViewById(R.id.tvRegister);
+
+        Info.setText("No of attempt remaining : 5");
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        progressDialog = new ProgressDialog(this);
+
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+
+        if(user != null) {
+            finish();
+            startActivity(new Intent(MainActivity.this, SecondActivity.class));
+        }
+
+
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                validate(Name.getText().toString(), Password.getText().toString());
+            }
+        });
+
+        userRegistration.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, RegistrationActivity.class));
+            }
+        });
+
+    }
+
+    private void validate(String userName, String userPassword) {
+
+        progressDialog.setMessage("Please Wait");
+        progressDialog.show();
+
+
+        firebaseAuth.signInWithEmailAndPassword(userName, userPassword).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+            if(task.isSuccessful()){
+                progressDialog.dismiss();
+                Toast.makeText(MainActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(MainActivity.this, SecondActivity.class));
+            }else{
+                Toast.makeText(MainActivity.this, "Login Failed", Toast.LENGTH_SHORT).show();
+                counter--;
+                Info.setText("No of attempt remaining : " + counter);
+                progressDialog.dismiss();
+                if(counter == 0) {
+                    Login.setEnabled(false);
+                }
+                }
+            }
+        });
+    }
+}
